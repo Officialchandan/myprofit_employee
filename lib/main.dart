6c355b99ad670b68722e3ede63ff592cd2eb4ad5
vaibhav gour <vaibhav.gour@tekzee.com>
2022-02-15 14:20:41 +0530

    commite only lib

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:myprofit_employee/provider/NavigationService.dart'
+import 'package:myprofit_employee/provider/NavigationService.dart';
 import 'package:myprofit_employee/src/ui/home/home.dart';
 import 'package:myprofit_employee/src/ui/splash/splash.dart';
 import 'package:flutter/material.dart';
